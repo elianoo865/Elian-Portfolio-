f94a66a4c8c349fd1b79dc46a684baeb0d5c51fa
elianoo865 <elian446kad@gmail.com>
2026-01-21 19:28:54 +0300

    Fix color Error

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class PortfolioApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final seed = const Color(0xFF#008080); // teal-blue
+    final seed = const Color(0xFF008080); // teal-blue
 
     final theme = ThemeData(
       useMaterial3: true,
